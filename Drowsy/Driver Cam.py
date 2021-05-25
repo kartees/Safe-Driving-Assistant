bdd1fff1ca5f5d108c5d6103ae0773d522b547a8
kartees <58431189+kartees@users.noreply.github.com>
2021-05-25 20:45:44 +0530

    Add files via upload

---
+++ Drowsy/Driver Cam.py
@@ -0,0 +1,104 @@
+import numpy
+from pygame import mixer
+import time
+import cv2
+from tkinter import *
+import tkinter.messagebox
+root=Tk()
+root.geometry('500x570')
+frame = Frame(root, relief=RIDGE, borderwidth=2)
+frame.pack(fill=BOTH,expand=1)
+root.title('Safe Driving Assistant')
+frame.config(background='grey')
+label = Label(frame, text="Safe Driving Assistant",bg='grey',font=('Times 35 bold'))
+label.pack(side=TOP)
+para = Label(frame, text="Detects whether you are sleepy or not ",bg='grey',font=('Times 14 bold'))
+para.pack(side=TOP)
+para.place(x=100,y=130)
+# filename = PhotoImage(file="demo.png")
+# background_label = Label(frame,image=filename)
+# background_label.pack(side=TOP)
+
+
+
+def hel():
+   help(cv2)
+
+def Contri():
+   tkinter.messagebox.showinfo("Contributors","\n1.Mayur Kadam\n2. Abhishek Ezhava \n3. Rajendra Patil \n")
+
+
+def anotherWin():
+   tkinter.messagebox.showinfo("About",'Driver Cam version v1.0\n Made Using\n-OpenCV\n-Numpy\n-Tkinter\n In Python 3')
+                                    
+   
+
+menu = Menu(root)
+root.config(menu=menu)
+
+subm1 = Menu(menu)
+menu.add_cascade(label="Tools",menu=subm1)
+subm1.add_command(label="Open CV Docs",command=hel)
+
+subm2 = Menu(menu)
+menu.add_cascade(label="About",menu=subm2)
+subm2.add_command(label="Driver Cam",command=anotherWin)
+subm2.add_command(label="Contributors",command=Contri)
+
+
+
+def exitt():
+   exit()
+ 
+def alert():
+   mixer.init()
+   alert=mixer.Sound('beep-07.wav')
+   alert.play()
+   time.sleep(0.1)
+   alert.play()   
+   
+def blink():
+   capture =cv2.VideoCapture(0)
+   face_cascade = cv2.CascadeClassifier('lbpcascade_frontalface.xml')
+   eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+   blink_cascade = cv2.CascadeClassifier('CustomBlinkCascade.xml')
+
+   while True:
+      ret, frame = capture.read()
+      gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+      faces = face_cascade.detectMultiScale(gray)
+
+      for (x,y,w,h) in faces:
+         font = cv2.FONT_HERSHEY_COMPLEX
+         cv2.putText(frame,'Face',(x+w,y+h),font,1,(250,250,250),2,cv2.LINE_AA)
+         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+         roi_gray = gray[y:y+h, x:x+w]
+         roi_color = frame[y:y+h, x:x+w]
+
+         eyes = eye_cascade.detectMultiScale(roi_gray)
+         for(ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+         blink = blink_cascade.detectMultiScale(roi_gray)
+         for(eyx,eyy,eyw,eyh) in blink:
+            cv2.rectangle(roi_color,(eyx,eyy),(eyx+eyw,eyy+eyh),(255,255,0),2)
+            alert()
+      cv2.imshow('frame',frame)
+      if cv2.waitKey(1) & 0xFF ==ord('q'):
+          break
+         
+  
+   capture.release()
+   cv2.destroyAllWindows()
+
+   
+
+but5=Button(frame,padx=5,pady=5,width=20,bg='white',fg='black',relief=GROOVE,command=blink,text='Drowsiness Detection',font=('helvetica 15 bold'))
+but5.place(x=130,y=170)
+
+but5=Button(frame,padx=5,pady=5,width=5,bg='white',fg='black',relief=GROOVE,text='EXIT',command=exitt,font=('helvetica 15 bold'))
+but5.place(x=210,y=478)
+
+
+root.mainloop()
+
